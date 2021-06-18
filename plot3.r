94bb441460724fad2d7d6dfe3cf2679f299043ff
pi-develop <joshuazyli@gmail.com>
2021-06-18 12:16:52 -1000

    Create plot3.r

---
+++ plot3.r
@@ -0,0 +1,5 @@
+energy2<-read.table(text = grep("^[1,2]/2/2007", readLines("C:\\Users\\joshu\\Desktop\\Rcoursera\\exdata_data_household_power_consumption\\household_power_consumption.txt"), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
+plot(x=energydate, y=energy2$Sub_metering_1, type="l", xlab="",ylab="Energy Sub Metering")
+lines(x=energydate,y=energy2$Sub_metering_2,col="Red")
+lines(x=energydate,y=energy2$Sub_metering_3,col="Blue")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("Black","Red","Blue"),lty=1)
