d3797e8935e8cc2f69702db8e595e9568e8472a6
pi-develop <joshuazyli@gmail.com>
2021-06-18 12:15:27 -1000

    Create plot1.r

---
+++ plot1.r
@@ -0,0 +1,2 @@
+energy <- read.table(text = grep("^[1,2]/2/2007", readLines("C:\\Users\\joshu\\Desktop\\Rcoursera\\exdata_data_household_power_consumption\\household_power_consumption.txt"), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
+hist(energy$Global_active_power,main="Global Active Power",xlab="Global Active Power(Kilowatts)", ylim=c(0,1400),col="Red")
